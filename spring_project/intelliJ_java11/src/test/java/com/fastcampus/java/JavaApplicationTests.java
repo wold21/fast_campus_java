5ed7b0cbb826dcd4ece1d0a74a87af15e1a6d757
wold21 <wold2180@gmail.com>
2021-02-14 16:51:32 +0900

    강의가 버전관리가 안돼서 곧 나올 리뉴얼 버전으로 다시 시작할예정.

--- spring_project/intelliJ_java11/src/test/java/com/fastcampus/java/JavaApplicationTests.java
+++ spring_project/intelliJ_java11/src/test/java/com/fastcampus/java/JavaApplicationTests.java
@@ -2,8 +2,10 @@ package com.fastcampus.java;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @DataJpaTest
+@WebAppConfiguration
 public class JavaApplicationTests {
 
     @Test
