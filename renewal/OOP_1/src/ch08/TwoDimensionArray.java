11e4a7e9c4866002d56b91181bf0e87ae58b2c1e
wold21 <wold2180@gmail.com>
2021-04-11 10:42:33 +0900

    TwoDimension Array

--- renewal/OOP_1/src/ch08/TwoDimensionArray.java
+++ renewal/OOP_1/src/ch08/TwoDimensionArray.java
@@ -5,7 +5,7 @@ public class TwoDimensionArray {
         int i, j;
         for (i = 0; i < arr.length; i++) {
             for (j = 0; j < arr[i].length; j++) {
-                System.out.println(arr[i][j] + ",");
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println("\t" + arr[i].length);
         }
